02d7727c04d49eaff85cd914044bba233fd254fb
Brandonlee Santos <64438740+diracdyson@users.noreply.github.com>
2023-01-21 03:02:03 -0500

    Add files via upload

---
+++ pytorch/utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 21:36:17 2022
+
+@author: brsantos
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_learning_curve(x, scores, figure_file):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 100 scores')
+    plt.savefig(figure_file)
